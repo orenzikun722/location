731f5fe2c37c6fc2535599d9b54643212f3e5686
orenzikun722 <164862462+orenzikun722@users.noreply.github.com>
2025-01-11 18:48:13 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-
+import {system} from "@minecraft/server";
+system.runInterval(() => {
+	for (const player of server.world.getAllPlayers()){
+    const x = player.location.x;
+    const y = player.location.y;
+    const z = player.location.z;
+    const r = player.getRotation().y;
+    player.runCommandAsync(`titleraw @a actionbar{"rawtext":[{"text": "XYZ: ${x}, ${y}, ${z}"}]}`);
+  }
+});
